d5b9643d0ba1ed1a9b0f2d9155ddfb81f9ef49ab
Jose Luque <joseluque93@hotmail.com>
2020-05-20 21:11:50 -0500

    fix book view

--- src/database.ts
+++ src/database.ts
@@ -6,3 +6,13 @@ mongoose.connect(mongodb.URI, {
   useNewUrlParser: true
 }).then(db => console.log('Db is connected'))
   .catch(err => console.log(err));
+
+//   const mongoose = require('mongoose');
+
+// const { mongodb } = require('./keys');
+
+// mongoose.connect(mongodb.URI, {
+//     useNewUrlParser: true
+// })
+//     .then((db:any) => console.log('DB is connected'))
+//     .catch((err: any) => console.error(err));
